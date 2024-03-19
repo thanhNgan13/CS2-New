ee81db83779ebebf69db50340a8efc496eb45a27
Phan Thành Ngân <nngann2402@gmail.com>
2024-03-19 10:04:11 +0700

    login

---
+++ lib/src/views/pages/users/setting.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatefulWidget {
+  const SettingPage({super.key});
+
+  @override
+  State<SettingPage> createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Setting'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Setting Page',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
